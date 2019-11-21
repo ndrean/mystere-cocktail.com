6d1f86118089d8097d0152becd574d2cc082283f
Neven DREAN <nevendrean@yahoo.fr>
2019-11-21 15:15:41 +0100

    new cocktail sans dose

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  #root to: 'cocktails#index'
+  resources :cocktails, only: [:index, :show, :new, :create]
+  # get '/cocktails', to: 'cocktails#index'
+  # get 'cocktails/show'
+  # get 'cocktails/new'
+  # get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+Ingredient.destroy_all
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+file = open(url).read
+file_list = JSON.parse(file)
+file_list['drinks'].each do |list|
+  Ingredient.create!(name: list['strIngredient1'])
+  end
+
+Cocktail.create!(name: 'Marijeanne')
+Cocktail.create!(name: 'La Blanche')
+Cocktail.create!(name: 'Sex on the beach')
